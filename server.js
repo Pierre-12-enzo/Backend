2a5ab64bef5f1ce75cb268656016956813a78a3d
Pierre-12-enzo <dusenge.enzo87@gmail.com>
2024-11-13 19:59:20 +0200

    has ch

--- server.js
+++ server.js
@@ -27,5 +27,5 @@ app.use('/api', routes);
 // Start the server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-    console.log(`Server runnings on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
