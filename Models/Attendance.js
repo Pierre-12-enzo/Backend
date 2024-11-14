a357f98a0ddccc99654cd099441b9ab86ff090d8
Pierre-12-enzo <dusenge.enzo87@gmail.com>
2024-11-14 16:25:35 +0200

    changed

--- Models/Attendance.js
+++ Models/Attendance.js
@@ -4,7 +4,8 @@ const attendanceSchema = new mongoose.Schema({
   student_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Student', required: true },
   session_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Session', required: true },
   status: { type: String, enum: ['present', 'absent'], required: true },
-  comment: { type: String, default: '' } // Reason for absence, if any
+  comment: { type: String, default: '' }, // Reason for absence, if any
+  date: { type: Date, required: true }
 }, { timestamps: true });
 
 module.exports = mongoose.model('Attendance', attendanceSchema);
