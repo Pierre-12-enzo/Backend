eff67982c9cd4ef65806e4689ff02992a9b3b629
Pierre-12-enzo <dusenge.enzo87@gmail.com>
2024-11-14 16:25:15 +0200

    changed

--- Models/Session.js
+++ Models/Session.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const sessionSchema = new mongoose.Schema({
   class_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Class', required: true },
   date: { type: Date, required: true },
-  attendance: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Student' }] // References to students who attended
+  attended: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Student' }] // References to students who attended
 }, { timestamps: true });
 
 module.exports = mongoose.model('Session', sessionSchema);
